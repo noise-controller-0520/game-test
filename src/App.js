22e9322b621bba2ba9710fe5713bbc60400d6840
jjgorton <jjgorton@gmail.com>
2019-05-23 13:26:01 -0400

    initial file dump into repo

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Audio from './Audio';
+
+import './App.css';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<Audio />
+			</div>
+		);
+	}
+}
+
+export default App;
